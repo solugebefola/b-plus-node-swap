950f33bd42d55684bad0bbb801418d353286cc3f
Solar Olugebefola <solar@iobeam.com>
2017-05-19 09:15:13 -0400

    fixup! Create node object

--- tree.js
+++ tree.js
@@ -1,28 +1,10 @@
-/*
-From http://www.cburch.com/cs/340/reading/btree/
 
---Every node has one more references than it has keys.
---All leaves are at the same distance from the root.
---For every non-leaf node N with k being the number of keys in N: all keys
-in the first child's subtree are less than N's first key; and all keys
-in the ith child's subtree (2 ≤ i ≤ k) are between the (i − 1)th key of
-n and the ith key of n.
---The root has at least two children.
---Every non-leaf, non-root node has at least floor(d / 2) children.
---Each leaf contains at least floor(d / 2) keys.
---Every key from the table appears in a leaf, in left-to-right sorted order.
-
-My implementation:
-Node class that stores: order, parent, if it is root, keys, children, nearest siblings
-Leaf class extends node, # children = # keys
-Root class extends node, min children = 2
-nodesInMem linked list of nodes stored in memory (length is set by user)
-
-Starting the tree, the first node is a Leaf until it has enough inserts to split.
-*/
 
 class Node {
-    constructor(order, nodesInMem, parent, isRoot = false, keyList = [], children = []) {
+    constructor(id, order, nodesInMem,
+        parent, isRoot = false, keyList = [], children = []) {
+
+        this.id = id;
         this.order = order;
         this.parent = parent;
         this.isRoot = isRoot;
@@ -53,7 +35,7 @@ class Node {
 
     removeSelf() {
         //if the node is merged, this removes it completely from the list
-        this.nodeList.remove(this);
+        this.nodesInMem.remove(this);
     }
 
     searchFor(key) {
@@ -65,7 +47,28 @@ class Node {
         return false;
     }
 
-    insert(key, value) {
-
+    insert(key, value, node = null) {
+        if (!node) {
+            // pass insert call down to the correct leaf
+            for (let i = 0; i < this.keyList.length; i++) {
+                if (key < this.keyList[i]) {
+                    this.children[i].insert(key, value);
+                    break;
+                }
+            }
+        } else {
+            // lower node is calling insert on this node
+            if (this.keyList.length < (this.order - 1)) {
+                // not full
+                let insertIndex = this.keyList.findIndex((val) => {
+                    return val > key;
+                });
+                this.keyList.splice(insertIndex, 0, key);
+                this.children.splice(insertIndex, 0, node);
+            } else {
+                // full and needs to split
+                ***
+            }
+        }
     }
 }
